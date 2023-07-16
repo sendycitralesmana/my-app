aaeea5d9cf06a3d58ee136b2ec0f1b2121d2c44b
Lesmana <sendycitralesmana@gmail.com>
2023-07-16 15:44:31 +0700

    StateLess component & StateFull component

---
+++ src/components/HelloComponent.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import './HelloComponent.css';
+
+const HelloComponent = () => {
+    return <p className="text-p">Stateless Component</p>
+}
+
+export default HelloComponent;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import HelloComponent from './components/HelloComponent';
+import StateFullComponent from './container/StateFullComponent';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HelloComponent />
   </React.StrictMode>
 );
 
