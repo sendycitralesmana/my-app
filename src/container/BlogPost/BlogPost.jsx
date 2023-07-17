1cd781dd7051f381e80e5b9160e4a0737859e68b
Lesmana <sendycitralesmana@gmail.com>
2023-07-17 18:59:44 +0700

    Interaksi dengan back-end -pemanggilan API (get) part 2

--- src/container/BlogPost/BlogPost.jsx
+++ src/container/BlogPost/BlogPost.jsx
@@ -1,12 +1,41 @@
 import { Component, Fragment } from "react";
 import Post from "../Post/Post";
+import axios from "axios";
 
 class BlogPost extends Component {
+
+    state = {
+        post: []
+    }
+
+    componentDidMount() {
+        // cara 1
+        // fetch('https://jsonplaceholder.typicode.com/posts')
+        // .then(response => response.json())
+        // .then(json => {
+        //     this.setState({
+        //         post: json
+        //     })
+        // })
+
+        // cara 2 -> $ npm install axios
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then((result) => {
+            this.setState({
+                post: result.data
+            })
+        })
+    }
+
     render () {
         return (
             <Fragment>
                 <p>Blog Post</p>
-                <Post tittle="title" desc="desc" />
+                {
+                    this.state.post.map(post => {
+                        return <Post key={post.id} title={post.title} desc={post.body} />
+                    })
+                }
             </Fragment>
         )
     }
--- src/container/Post/Post.jsx
+++ src/container/Post/Post.jsx
@@ -7,8 +7,8 @@ const Post = (props) => {
                 <img src="" alt="" />
             </div>
             <div className="content">
-                <p className="title">Title</p>
-                <p className="desc">Desc</p>
+                <p className="title">{props.title}</p>
+                <p className="desc">{props.desc}</p>
             </div>
         </div>
     )
